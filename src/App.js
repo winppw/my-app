cc8898dfaf56f60e7052e6305596ec5f3f15cdad
Peerapon <peerapon.ppw@gmail.com>
2020-08-19 05:41:02 +0700

    add button component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <Button/>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +24,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
